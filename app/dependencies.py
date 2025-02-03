33f7bad1e7185bbd218acedb20a8de5abe7efce5
Sushrut  Sadana <sushrutsd@gmail.com>
2025-02-03 08:02:43 -0800

    dependencies.py fix

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,4 +1,8 @@
 from fastapi import Depends
+from .services.lead_service import LeadService
+from .services.email_processor import EmailProcessor
+from .services.call_service import CallService
+from .services.email_service import EmailService
 from supabase import create_client
 import os
 
@@ -6,4 +10,17 @@ def get_supabase():
     return create_client(
         supabase_url=os.environ.get("SUPABASE_URL"),
         supabase_key=os.environ.get("SUPABASE_KEY")
-    ) 
+    )
+
+def get_lead_service(supabase=Depends(get_supabase)):
+    return LeadService(supabase)
+
+def get_email_processor(lead_service=Depends(get_lead_service)):
+    return EmailProcessor(lead_service)
+
+def get_email_service(supabase=Depends(get_supabase)):
+    return EmailService(supabase)
+
+def get_call_service(supabase=Depends(get_supabase), 
+                    email_service=Depends(get_email_service)):
+    return CallService(supabase, email_service)
